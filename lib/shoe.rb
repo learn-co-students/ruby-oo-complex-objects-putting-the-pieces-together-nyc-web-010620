582ddd0f12b14efab11f1fb7b1370f0651f260f9
shalva-an <s.halvaa26@gmail.com>
2020-01-08 08:52:57 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :color, :size, :material, :condition # both: setters and getters
+    attr_reader :brand # only set since get is already initialized
+   
+    def initialize(brand) # gets brand on run
+        @brand = brand
+    end
+
+
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+
+
+
+
+
+end
